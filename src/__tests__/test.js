97062c0b31d11baabee27e5ad65d01908604bc99
Naloyka <elena.naloeva@yandex.ru>
2022-07-14 21:08:08 +0300

    first commit

---
+++ src/__tests__/test.js
@@ -0,0 +1,26 @@
+import Zombie from "../index.js";
+
+test('create new class', () => {
+    const result = new Zombie('Helen', 'Zombie')
+
+    const sample = {
+        attack: 40,
+        defence: 10,
+        health: 100,
+        level: 100,
+        name: "Helen",
+        type: "Zombie"
+    }
+
+    expect(result).toEqual(sample);
+});
+
+test('catch the Error with name', () => {
+    const result1 = () => new Zombie('L', 'Zombie');
+    expect(result1).toThrow("Некорректное имя");
+  });
+  
+  test('catch the Error with type', () => {
+    const result2 = () => new Zombie('Lena', 'Character');
+    expect(result2).toThrow("Некорректный тип");
+  });
